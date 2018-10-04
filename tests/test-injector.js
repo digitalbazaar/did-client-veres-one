61c4dc13a47d412659e521c9225f137c215c39ea
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-10-04 19:18:15 -0400

    Extract v1 ledger client code from web-ledger-client and did-io.

---
+++ tests/test-injector.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const contexts = require('../lib/contexts')
+const Injector = require('../lib/Injector');
+const injector = new Injector();
+
+// FIXME: determine how to simplify/move this code out of test
+const jsonld = injector.use('jsonld');
+const documentLoader = jsonld.documentLoader;
+
+jsonld.documentLoader = async url => {
+  if(url in contexts) {
+    return {
+      contextUrl: null,
+      documentUrl: url,
+      document: contexts[url]
+    };
+  }
+  return documentLoader(url);
+};
+injector.use('jsonld', jsonld);
+
+injector.env = {nodejs: true};
+
+module.exports = injector;
